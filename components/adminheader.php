39ece0d2f6c61bdf04f71701585225b7992e2aef
Yasharth-Mishra <119748932+Yasharth-Mishra@users.noreply.github.com>
2023-08-27 14:06:21 +0530

    Add files via upload

---
+++ components/adminheader.php
@@ -0,0 +1,14 @@
+<header class="header">
+
+   <section class="flex">
+      <a href="#" class="logo">Logo</a>
+
+      <nav class="navbar">
+         <a href="add_product.php">add product</a>
+         <a href="view_productsA.php">view products</a>
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+   </section>
+
+</header>
